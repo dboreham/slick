093eb9d064d5abfb786358e2e912d9eacc829029
David Boreham <david@carbonhealth.com>
2021-08-18 20:24:40 -0600

    Log the field offset

--- slick/src/main/scala/slick/jdbc/JdbcMappingCompilerComponent.scala
+++ slick/src/main/scala/slick/jdbc/JdbcMappingCompilerComponent.scala
@@ -28,7 +28,7 @@ trait JdbcMappingCompilerComponent { self: JdbcProfile =>
     def createColumnConverter(n: Node, idx: Int, column: Option[FieldSymbol]): ResultConverter[JdbcResultConverterDomain, _] = {
       val JdbcType(ti, option) = n.nodeType.structural
       if(option) createOptionResultConverter(ti, idx)
-      else createBaseResultConverter(ti, column.fold("<computed>")(_.name), idx)
+      else createBaseResultConverter(ti, column.fold(s"<computed:${idx}>")(_.name), idx)
     }
 
     override def createGetOrElseResultConverter[T](rc: ResultConverter[JdbcResultConverterDomain, Option[T]], default: () => T) = rc match {
