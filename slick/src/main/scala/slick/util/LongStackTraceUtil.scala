a2fbe499f095cd272c0a8964fb2f83253bdf9330
David Boreham <dboreham@carbonhealth.com>
2021-09-15 19:39:27 -0600

    Working

--- slick/src/main/scala/slick/util/LongStackTraceUtil.scala
+++ slick/src/main/scala/slick/util/LongStackTraceUtil.scala
@@ -12,10 +12,16 @@ object LongStackTraceUtil {
 
       def getCurrentLongStackTrace(): LongStackTrace = {
             // Make a new object from the current stack trace and whatever is in TLS for ancestors
-            val currentStackTrace: StackTrace = Thread.currentThread().getStackTrace()
-            // TODO: chop the tail off here
+            // chop the top off here because it includes this function -- need to remove the top two elements
+            val currentStackTrace: StackTrace = Thread.currentThread().getStackTrace().drop(2)
             // This has to go in TLS in the child thread
-            return threadLocalAncestorStackTraces.value.appended(currentStackTrace)
+            threadLocalAncestorStackTraces.value match {
+                  case null => Array(currentStackTrace)
+                  case _ => {
+                        val separatorStackTrace = Array(new StackTraceElement("dummy", "", "", 1))
+                        threadLocalAncestorStackTraces.value.prependedAll(Array(currentStackTrace, separatorStackTrace))
+                  }
+            }
       }
 
       def setAncestorStackTrace(ancestorStackTraces: LongStackTrace) = {
@@ -23,11 +29,15 @@ object LongStackTraceUtil {
       }
 
       def createChainedStackTrace(childStackTrace: StackTrace): StackTrace = {
-        // merge and return
-        threadLocalAncestorStackTraces.value.flatten.appendedAll(childStackTrace)
+        chainStackTraces(threadLocalAncestorStackTraces.value.prepended(childStackTrace)).flatten
       }
 
       def clearAncestorStackTrace() = {
             threadLocalAncestorStackTraces.value = null
+      }
+
+      private def chainStackTraces(traces: LongStackTrace): LongStackTrace = {
+            val separatorStackTrace = Array(new StackTraceElement("Context-switch-->>>>", "", "", 1))
+            traces.flatMap( x => Array(x, separatorStackTrace))
       }
 }
